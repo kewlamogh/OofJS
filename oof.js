22a35d896fa5c3d8c4cab8d95e432825e8d90764
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-07 02:27:43 +0000

    fixed_bug

--- oof.js
+++ oof.js
@@ -6,6 +6,45 @@ class OofJS {
     this.foreverUpdate = foreverUpdate;
     this.pen = this.canvas.getContext("2d");
     this.objects = []; //objects to draw
+    this.keysPressed = {};
+    let k = []
+    document.onkeydown = function (e) {k[e.key] = true};
+    document.onkeyup = function (e) {delete k[e.key]};
+    this.keysPressed = k;
+  }
+  setStyleForCanvas(styling) {
+    for (var i in styling) {
+      this.canvas.style[i] = styling[i];
+    }
+  }
+  isPressed(key) {
+    return key /*is*/ in this.keysPressed
+  }
+  setAttr(name, p, nv) {
+    let selectedIndex;
+    for (var i = 0; i<=this.objects.length - 1;i ++) {
+      if (this.objects[i].name == name) {
+        selectedIndex = i;
+      }
+    }
+    if (selectedIndex == undefined) {
+      return null;
+    }
+    this.objects[selectedIndex][p] = nv;
+    return nv;
+  }
+  getAttr(name, p) {
+    let selectedIndex;
+    for (var i = 0; i<=this.objects.length - 1;i ++) {
+      if (this.objects[i].name == name) {
+        selectedIndex = i;
+      }
+    }
+    if (selectedIndex == undefined) {
+      return null;
+    } else {
+      return this.objects[selectedIndex][p]
+    }
   }
   inject() {
     document.body.appendChild(this.canvas); //injecting canvas
@@ -18,45 +57,54 @@ class OofJS {
       func(list[i]);
     }
   }
-  addObject(x, y, w, h, isImg = false, imgSource = "") {
-    if (!isImg) {
+  addObject(x, y, w, h, name, color = "black", isImg = false, imgSource = "") {
+    if (isImg == false) {
       this.objects.push({
         x: x,
         y: y,
         w: w,
-        h: h
+        h: h,
+        shape: 'rect',
+        name: name,
+        col: color
       })
-    } else {
+    } else if (isImg) {
       let img = document.createElement("img");
       img.src = imgSource;
       img.width = w;
       img.height = h;
-
       this.objects.push({
         x: x,
         y: y,
         img: true,
-        imgElement: img
+        imgElement: img,
+        shape: undefined,
+        name: name
       })
     }
   }
-  draw() {
-    this.eraseAll();
-    this.#iterate(this.objects, function (currentObject) {
+  render() {
+    this.draw(this);
+  }
+  draw(inst) {
+    inst.eraseAll();
+    inst.#iterate(inst.objects, function (currentObject) {
       if (currentObject.hasOwnProperty("img")) {
-        this.pen.drawImage(currentObject.imgElement, currentObject.x, currentObject.y);
+        inst.pen.drawImage(currentObject.imgElement, currentObject.x, currentObject.y);
       }
       if (currentObject.shape == "rect") {
-        this.pen.fillRect(currentObject.w, currentObject.h, currentObject.x, currentObject.y);
+        inst.pen.fillStyle = currentObject.col;
+        inst.pen.fillRect(currentObject.x, currentObject.y, currentObject.w, currentObject.h);
       }
     })
-    this.callback();
+    inst.callback(inst);
   }
-  callback() {
-    if (this.foreverUpdate) {
-      requestAnimationFrame(this.draw); //requestAnimationFrame creates a bad scope
-      //if I try manual callback it breaks (the page doesn't load)
-      //and i might get an exeption :( (max callback stack exceeded)
+  callback(inst) {
+    if (inst.foreverUpdate) {
+      requestAnimationFrame(function () {inst.draw(inst)}); 
     }
+  }
+  onclick(func) {
+    this.canvas.addEventListener("click", function (e) {func(e)})
   }
 }
